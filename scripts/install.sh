c26a164c5b05e6cd5cfbcf8ed0b9ab056140e2df
boku-ha-saboten <kakemasato1211@gmail.com>
2025-05-08 22:40:14 +0900

    一旦削除

--- scripts/install.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-set -ex
-
-TRRAFORM_VERSION="1.11.2"
-
-wget -q "https://releases.hashicorp.com/terraform/${TERRAFORM_VERSION}/terraform_${TERRAFORM_VERSION}_linux_amd64.zip"
-unzip -d /usr/local/bin "terraform_${TERRAFORM_VERSION}_linux_amd64.zip"
