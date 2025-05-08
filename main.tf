80c9d5aa16970ab3b0b72b8138b6bedd7519aab6
boku-ha-saboten <kakemasato1211@gmail.com>
2025-05-08 23:16:36 +0900

    途中まで

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 #####################################
-#VPC
+# VPC
 #####################################
 resource "aws_vpc" "my_vpc" {
   cidr_block = "192.168.0.0/16"
@@ -13,7 +13,7 @@ resource "aws_vpc" "my_vpc" {
   }
 }
 #####################################
-#サブネット
+# サブネット
 #####################################
 resource "aws_subnet" "public_subnet1a" {
   vpc_id            = aws_vpc.my_vpc.id
@@ -52,8 +52,41 @@ resource "aws_subnet" "public_subnet1d" {
   }
 }
 
+resource "aws_subnet" "private_subnet1a" {
+  vpc_id            = aws_vpc.my_vpc.id
+  cidr_block        = "192.168.3.0/24"
+  availability_zone = "ap-northeast-1a"
+  
+  tags = {
+    Name   = "private-subnet1a"
+    Create = "terraform"
+  }
+}
+
+resource "aws_subnet" "private_subnet1c" {
+  vpc_id            = aws_vpc.my_vpc.id
+  cidr_block        = "192.168.4.0/24"
+  availability_zone = "ap-northeast-1c"
+  
+  tags = {
+    Name   = "private-subnet1c"
+    Create = "terraform"
+  }
+}
+
+resource "aws_subnet" "private_subnet1d" {
+  vpc_id            = aws_vpc.my_vpc.id
+  cidr_block        = "192.168.5.0/24"
+  availability_zone = "ap-northeast-1d"
+  
+  tags = {
+    Name   = "private-subnet1d"
+    Create = "terraform"
+  }
+}
+
 #####################################
-#ルートテーブル
+# ルートテーブル
 #####################################
 resource "aws_route_table" "public_route_table" {
   vpc_id = aws_vpc.my_vpc.id
@@ -82,7 +115,7 @@ resource "aws_route_table_association" "public_association" {
 }
 
 #####################################
-#インターネットゲートウェイ
+# インターネットゲートウェイ
 #####################################
 resource "aws_internet_gateway" "internet_gateway" {
   vpc_id = aws_vpc.my_vpc.id
@@ -92,3 +125,60 @@ resource "aws_internet_gateway" "internet_gateway" {
     Create = "terraform"
   }
 }
+
+#####################################
+# Security Group
+#####################################
+resource "aws_security_group" "rds_sg" {
+  name_prefix = "rds-sg-"
+  description = "Security Group for RDS"
+  vpc_id      = aws_vpc.my_vpc.id
+
+  ingress {
+    cidr_blocks = aws_subnet.public_subnet1a.cidr_block
+    from_port   = 5432
+    to_port     = 5432
+    protocol    = "tcp"
+  }
+
+  ingress {
+    cidr_blocks = aws_subnet.public_subnet1c.cidr_block
+    from_port   = 5432
+    to_port     = 5432
+    protocol    = "tcp"
+  }
+
+  ingress {
+    cidr_blocks = aws_subnet.public_subnet1d.cidr_block
+    from_port   = 5432
+    to_port     = 5432
+    protocol    = "tcp"
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name   = "rds-security-group"
+    Create = "terraform"
+  }
+}
+
+#####################################
+# 踏み台EC2
+#####################################
+resource "aws_instance" "my_instance" {
+  ami           = "ami-00d101850e971728d" # ap-northeast-1, SSD, Amazon Linux 2
+  instance_type = "t2.micro"
+  subnet_id     = aws_subnet.private_subnet1a
+  #今度書く
+}
+
+#####################################
+# RDS
+#####################################
+#今度書く
